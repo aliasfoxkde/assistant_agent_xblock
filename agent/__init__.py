96494b36a0bbc65f5b6e80501e1d65093a00772a
Micheal Kinney <micheal.l.c.kinney@gmail.com>
2025-04-04 20:21:11 -0500

    Cleanup.

--- agent/__init__.py
+++
@@ -1,3 +0,0 @@
-from .chatgpt_block import ChatGPTBlock
-
-__version__ = '0.1.6'
--- temp/__init.py
+++
@@ -1,5 +0,0 @@
-"""Voice Assistance XBlock for OpenEdX using VAPI.ai."""
-from .voice_assistance import VoiceAssistanceXBlock
-
-if __name__ == "__main__":
-    pass
